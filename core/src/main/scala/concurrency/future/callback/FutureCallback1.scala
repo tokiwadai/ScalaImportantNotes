ae4f58844f15ade9f5e6b5b0fa2e8bc93cff6858
tokiwadai <owen16.ho@gmail.com>
2017-11-12 23:07:34 -0500

    added concurrent and implicit samples

---
+++ core/src/main/scala/concurrency/future/callback/FutureCallback1.scala
@@ -0,0 +1,44 @@
+package concurrency.future.callback
+
+import concurrency.sleep
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+
+object FutureCallback1App {
+  def main(args: Array[String]) = {
+    val fcb1 = new FutureCallback1
+    fcb1.run(new Session)
+    sleep(2000)
+    println("done")
+  }
+}
+
+class FutureCallback1 {
+  import scala.concurrent.ExecutionContext.Implicits.global
+  def run(session: Session) = {
+    val f: Future[List[String]] = Future {
+      session.getRecentPosts
+    }
+
+    f onComplete {
+      case Success(posts) => for (post <- posts) {
+        println("Success...")
+        println(s"post: $post")
+      }
+      case Failure(t) => println("An error has occured: " + t.getMessage)
+    }
+  }
+
+}
+
+class Session {
+  def getRecentPosts: List[String] = {
+    sleep(1000)
+    List("A", "B", "C")
+  }
+}
+
+
+
---
+++ core/src/main/scala/implicit/function/type/Transaction.scala
@@ -0,0 +1,51 @@
+package `implicit`.function.`type`
+
+import scala.collection.mutable.ListBuffer
+
+class Transaction {
+  private val log = new ListBuffer[String]
+  def println(s: String): Unit = log += s
+
+  private var aborted = false
+  private var committed = false
+
+  def abort(): Unit = { aborted = true }
+  def isAborted = aborted
+
+  def commit(): Unit =
+    if (!aborted && !committed) {
+      Console.println("******* log ********")
+      log.foreach(Console.println)
+      committed = true
+    }
+}
+
+object MyApp {
+  def transaction[T](op: Transaction => T) = {
+    val trans: Transaction = new Transaction
+    op(trans)
+    trans.commit()
+  }
+
+  def f1(x: Int)(implicit thisTransaction: Transaction): Int = {
+    thisTransaction.println(s"first step: $x")
+    f2(x + 1)
+  }
+  def f2(x: Int)(implicit thisTransaction: Transaction): Int = {
+    thisTransaction.println(s"second step: $x")
+    f3(x * x)
+  }
+  def f3(x: Int)(implicit thisTransaction: Transaction): Int = {
+    thisTransaction.println(s"third step: $x")
+    if (x % 2 != 0) thisTransaction.abort()
+    x
+  }
+
+  def main(args: Array[String]) = {
+    transaction {
+      implicit thisTransaction =>
+        val res = f1(args.length)
+        println(if (thisTransaction.isAborted) "aborted" else s"result: $res")
+    }
+  }
+}
